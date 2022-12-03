1cc5901e150da08e344b730ee7ed3010caa18983
roman_zh1 <roman_zh1@vk.com>
2022-12-03 15:53:27 +0300

    feat: add etg group tasks

---
+++ etg-group-ostrovok/channels.go
@@ -0,0 +1,40 @@
+package main
+
+import "sync"
+
+// task to implement the function
+func merge(chs ...chan int) chan int {
+
+}
+
+// solve
+func merge(chs ...chan int) chan int {
+	newCh := make(chan int)
+	wg := sync.WaitGroup{}
+
+	for _, ch := range chs {
+		wg.Add(1)
+		go func(ch chan int) {
+			for val := range ch {
+				newCh <- val
+			}
+			wg.Done()
+		}(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(newCh)
+	}()
+
+	return newCh
+}
+
+func main() {
+	ch1 := startProducerA()
+	ch2 := startProducerB()
+
+	for el := range merge(ch1, ch2) {
+		println(el)
+	}
+}
