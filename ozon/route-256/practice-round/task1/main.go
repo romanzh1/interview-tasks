bcc78f6383b332a915b26814d70b9a4bdede1b6f
zhiharev.roman2 <zhiharev.roman2@wb.ru>
2024-04-26 15:20:33 +0300

    feat: tasks for ozon

---
+++ ozon/route-256/practice-round/task1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var in *bufio.Reader
+	var out *bufio.Writer
+	in = bufio.NewReader(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var a, b int
+	fmt.Fscan(in, &a, &b)
+	fmt.Fprint(out, a+b)
+}
