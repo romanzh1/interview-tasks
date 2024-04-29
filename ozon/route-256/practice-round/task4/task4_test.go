3f555593018634be93a3e8ddb8c900d3b47c0e90
zhiharev.roman2 <zhiharev.roman2@wb.ru>
2024-04-29 18:33:12 +0300

    feat: add task4

---
+++ ozon/route-256/practice-round/task4/task4_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCalculateString(t *testing.T) {
+	for i := 1; i <= 20; i++ {
+		inputFile := "test_files/" + strconv.Itoa(i)
+		outputFile := "test_files/" + strconv.Itoa(i) + ".a"
+
+		input, err := os.Open(inputFile)
+		defer input.Close()
+		require.NoError(t, err, "Error opening input file: %s", inputFile)
+
+		expectedOutput, err := os.ReadFile(outputFile)
+		require.NoError(t, err, "Error opening output file: %s", outputFile)
+
+		reader := bufio.NewReader(input)
+		result := calculateString(reader)
+
+		require.Equal(t, strings.Split(string(expectedOutput), "\n"), strings.Split(result, "\n"),
+			"Test case %d", i)
+	}
+}
